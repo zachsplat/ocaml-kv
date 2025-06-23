cfb6f41fa364721c55faca01682dff15edc217bf
Zachary Alexander <86435089+zachsplat@users.noreply.github.com>
2025-06-22 21:15:00 -0500

    add TTL support with lazy expiry

--- lib/store.ml
+++ lib/store.ml
@@ -1,19 +1,42 @@
+type entry = {
+  value : string;
+  mutable expires_at : float option;
+}
+
 type t = {
-  mutable data : (string, string) Hashtbl.t;
+  mutable data : (string, entry) Hashtbl.t;
   mutable count : int;
 }
 
 let create () =
   { data = Hashtbl.create 256; count = 0 }
 
-let get t key =
-  Hashtbl.find_opt t.data key
+let now () = Unix.gettimeofday ()
 
-let set t key value =
+let is_expired e =
+  match e.expires_at with
+  | None -> false
+  | Some t -> now () > t
+
+let get t key =
+  match Hashtbl.find_opt t.data key with
+  | Some e when not (is_expired e) -> Some e.value
+  | Some _ ->
+    (* lazy cleanup *)
+    Hashtbl.remove t.data key;
+    t.count <- t.count - 1;
+    None
+  | None -> None
+
+let set ?ttl t key value =
+  let expires_at = match ttl with
+    | None -> None
+    | Some secs -> Some (now () +. secs)
+  in
   (match Hashtbl.find_opt t.data key with
    | None -> t.count <- t.count + 1
    | Some _ -> ());
-  Hashtbl.replace t.data key value
+  Hashtbl.replace t.data key { value; expires_at }
 
 let del t key =
   if Hashtbl.mem t.data key then begin
@@ -24,3 +47,13 @@ let del t key =
     false
 
 let size t = t.count
+
+(* remove all expired keys, called periodically maybe *)
+let gc t =
+  let to_remove = Hashtbl.fold (fun k e acc ->
+    if is_expired e then k :: acc else acc
+  ) t.data [] in
+  List.iter (fun k ->
+    Hashtbl.remove t.data k;
+    t.count <- t.count - 1
+  ) to_remove
--- lib/store.mli
+++ lib/store.mli
@@ -1,6 +1,7 @@
 type t
 val create : unit -> t
 val get : t -> string -> string option
-val set : t -> string -> string -> unit
+val set : ?ttl:float -> t -> string -> string -> unit
 val del : t -> string -> bool
 val size : t -> int
+val gc : t -> unit
