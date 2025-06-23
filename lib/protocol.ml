bb96233897880879aba189425a4a4a33273744ab
Zachary Alexander <86435089+zachsplat@users.noreply.github.com>
2025-06-22 21:22:00 -0500

    SET EX ttl_seconds support

--- lib/protocol.ml
+++ lib/protocol.ml
@@ -1,30 +1,26 @@
-(* dead simple text protocol. one command per line.
-   SET key value
-   GET key
-   DEL key
-   PING
-   QUIT
-*)
-
 type cmd =
-  | Set of string * string
+  | Set of string * string * float option  (* key, value, optional ttl *)
   | Get of string
   | Del of string
   | Ping
   | Quit
+  | Keys
   | Unknown of string
 
 let parse_line line =
   let parts = String.split_on_char ' ' (String.trim line) in
   match parts with
-  | ["SET"; k; v] -> Set (k, v)
+  | ["SET"; k; v] -> Set (k, v, None)
+  | ["SET"; k; v; "EX"; ttl_s] ->
+    (try Set (k, v, Some (float_of_string ttl_s))
+     with _ -> Unknown line)
   | "SET" :: k :: rest ->
-    (* value might have spaces *)
-    Set (k, String.concat " " rest)
+    Set (k, String.concat " " rest, None)
   | ["GET"; k] -> Get k
   | ["DEL"; k] -> Del k
   | ["PING"] | ["ping"] -> Ping
   | ["QUIT"] | ["quit"] -> Quit
+  | ["KEYS"] | ["keys"] -> Keys
   | _ -> Unknown line
 
 let response_ok = "+OK\n"
