03436254377527c7585153f4ec6668fbb2c59a0b
Zachary Alexander <86435089+zachsplat@users.noreply.github.com>
2025-07-01 23:02:00 -0500

    wire up persistence to main

--- bin/main.ml
+++ bin/main.ml
@@ -1,8 +1,17 @@
 let () =
   let port = ref 6380 in
+  let data_dir = ref "/tmp/ocaml-kv" in
   let spec = [
     ("--port", Arg.Set_int port, "port to listen on (default 6380)");
+    ("--data", Arg.Set_string data_dir, "data directory");
   ] in
-  Arg.parse spec (fun _ -> ()) "ocaml-kv [--port N]";
+  Arg.parse spec (fun _ -> ()) "ocaml-kv [--port N] [--data DIR]";
+  (try Unix.mkdir !data_dir 0o755 with Unix.Unix_error (Unix.EEXIST, _, _) -> ());
+  let aof_path = Filename.concat !data_dir "appendonly.log" in
   let store = Kv.Store.create () in
+  let aof = Kv.Aof.create aof_path in
+  Kv.Aof.replay aof store;
+  Kv.Aof.open_log aof;
+  Printf.printf "data dir: %s\n%!" !data_dir;
+  (* TODO: pass aof to server so it can log mutations *)
   Kv.Server.start ~port:!port store
